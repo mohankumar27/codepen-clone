174b220cd20cd9270c4d4a3f116c1c4d620d66e8
Mohan Kumar Sagadevan <mohankumar27197@gmail.com>
2020-09-20 22:25:48 +0530

    Updates

---
+++ precache-manifest.1fd7bed83c356d80b7fe32aa84af730d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f987ba930e32a02713164e0f30fbbed",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "6d4d45674fdf3694be77",
+    "url": "/codepen-clone/static/css/main.cdb39430.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4d45674fdf3694be77",
+    "url": "/codepen-clone/static/js/main.2bf63d99.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
