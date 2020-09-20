7f99e28eeaf6a9aa73f86d18207208b2b64721e0
Mohan Kumar Sagadevan <mohankumar27197@gmail.com>
2020-09-20 22:21:15 +0530

    Codepen clone (Initial commit)

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
+
+function Editor({ displayName, value, language, onChange }) {
+  const [open, setOpen] = useState(true);
+
+  const changeHandler = (value) => {
+    onChange(value);
+  };
+
+  return (
+    <div className={`editor ${open ? "" : "collapse"}`}>
+      <div className="editor__title">
+        {displayName}
+        <button
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+          type="button"
+          className="editor__expand-collapse"
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={changeHandler}
+        value={value}
+        className="editor__code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: "material",
+          lineNumbers: true,
+        }}
+      />
+    </div>
+  );
+}
+
+export default Editor;
